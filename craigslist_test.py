a3c87e640003a9cc9406fc4251d6e82c917d4acb
heiden12 <heiden12@github.com>
2016-11-13 22:57:10 -0500

    added comments.

--- craigslist_test.py
+++ craigslist_test.py
@@ -1,26 +1,21 @@
 import requests
 from bs4 import BeautifulSoup as bs4
 
+#Allow user to enter vehicle to search for
 print("What vehcile would you like to search for?(make+model)")
 search = input()
 
-#url = 'http://columbus.craigslist.org/search/cto?query=porsche+944'
 url = 'http://columbus.craigslist.org/search/cto?query=' + search
 rsp= requests.get(url)
-
-#print(rsp.text)
-
 html = bs4(rsp.text, 'html.parser')
-
 cars = html.body.find_all('li', attrs={'class': 'result-row'})
+
+#Print the number of results found
 print(len(cars), " Vehicles found")
 
-car_1 =  cars[0]
 #print(car_1.prettify())
 
-#price = car_1.findAll(attrs={'class': 'result-price'})[0].text
-#print(price)
-
+#Return the price of each vehicle returned by search criteria
 for x in range(0,len(cars)):
     price = cars[x].findAll(attrs={'class': 'result-price'})[0].text
     print(price)
