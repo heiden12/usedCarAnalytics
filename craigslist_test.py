3a8ef89df8fa073d8be5e185b1b8897f276d8a54
heiden12 <heiden12@github.com>
2016-11-13 22:55:06 -0500

    Added ability to input search criteria. Changed the output to include all search results.

--- craigslist_test.py
+++ craigslist_test.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs4
 
-url = 'http://columbus.craigslist.org/search/cto?query=porsche+944'
+print("What vehcile would you like to search for?(make+model)")
+search = input()
+
+#url = 'http://columbus.craigslist.org/search/cto?query=porsche+944'
+url = 'http://columbus.craigslist.org/search/cto?query=' + search
 rsp= requests.get(url)
 
 #print(rsp.text)
@@ -9,10 +13,14 @@ rsp= requests.get(url)
 html = bs4(rsp.text, 'html.parser')
 
 cars = html.body.find_all('li', attrs={'class': 'result-row'})
-#print(len(cars))
+print(len(cars), " Vehicles found")
 
 car_1 =  cars[0]
 #print(car_1.prettify())
 
-price = car_1.findAll(attrs={'class': 'result-price'})[0].text
-print(price)
+#price = car_1.findAll(attrs={'class': 'result-price'})[0].text
+#print(price)
+
+for x in range(0,len(cars)):
+    price = cars[x].findAll(attrs={'class': 'result-price'})[0].text
+    print(price)
