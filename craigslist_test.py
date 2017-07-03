432a20df721a12de1b6d5089c70c7c7914f43054
heiden12 <heiden12@github.com>
2017-07-03 16:54:59 -0400

    changes.

--- craigslist_test.py
+++ craigslist_test.py
@@ -9,11 +9,13 @@ url = 'http://columbus.craigslist.org/search/cto?query=' + search
 rsp= requests.get(url)
 html = bs4(rsp.text, 'html.parser')
 cars = html.body.find_all('li', attrs={'class': 'result-row'})
+carsAttrib = html.body.find_all('li', attrs={'class': 'attrgroup'})
 
 #Print the number of results found
 print(len(cars), " Vehicles found")
 
-#print(car_1.prettify())
+print(carsAttrib.prettify())
+#print(cars[0].prettify())
 
 #Return the price of each vehicle returned by search criteria
 for x in range(0,len(cars)):
